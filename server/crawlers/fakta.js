e2117fd8d6acf18119afe395a1b6928f3ad7fa95
Martin Gergov <martingergov1@gmail.com>
2014-11-27 12:09:09 +0100

    Fixed fakta cralw.

---
+++ server/crawlers/fakta.js
@@ -0,0 +1,25 @@
+var Crawler = require("crawler");
+var url = require('url');
+
+var site = "http://www.fakta.dk/tilbudsavis/produkter/tilbud.html";
+
+/*
+  Crawl function.
+  cb must accept one argument - the data.
+  data is in the form [{raw item}, {raw item}, {raw item}]
+*/
+function crawl(cb){
+    var nettoCrawler = new Crawler({
+	maxConnections : 10,
+	// This will be called for each crawled page
+	callback : function (error, result, $) {
+	    var Fakta = {Session:{}};
+	    eval($("script")['9'].children[0].data);
+	    cb(Fakta.Session.products);
+	}
+    });
+
+    nettoCrawler.queue(site);
+}
+
+module.exports = {crawl: crawl}
