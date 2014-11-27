74d025fcaf717f8441b7730f146bc20a71942b43
sallas <tbc105@hotmail.com>
2014-11-27 10:17:43 +0100

    protractor test added for logging in.

--- test/e2e-tests/scenarios.js
+++ test/e2e-tests/scenarios.js
@@ -5,41 +5,42 @@ var mongoose = require("mongoose");
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
+describe('my app', function () {
 
-  browser.get('/');
+    browser.get('/');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
-  });
-
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser.get('#/view1');
+    it('should automatically redirect to /view1 when location hash/fragment is empty', function () {
+        expect(browser.getLocationAbsUrl()).toMatch("/view1");
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    describe('view1', function () {
+
+        beforeEach(function () {
+            browser.get('#/view1');
+        });
+
+
+        it('should render view1 when user navigates to /view1', function () {
+            element(by.model("user.username")).sendKeys('student');
+            element(by.model("user.password")).sendKeys('test');
+            element(by.id('loginbtn')).click()
+                .then(function () {
+                    element(by.id("currentuserdisplay")).getText()
+                        .then(function (text) {
+                            expect(text).toEqual("Logged on as: Bo the Student");
+                        });
+                });
+        });
+
     });
 
-  });
 
+    describe('view2', function () {
 
-  describe('view2', function() {
+        beforeEach(function () {
+            browser.get('#/view2');
+        });
 
-    beforeEach(function() {
-      browser.get('#/view2');
     });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
