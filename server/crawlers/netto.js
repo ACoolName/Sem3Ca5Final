75d60ed0a5704ff38bf2cad8af4d374565429e34
sallas <tbc105@hotmail.com>
2014-11-28 13:27:11 +0100

    Added netto injector

--- server/crawlers/netto.js
+++ server/crawlers/netto.js
@@ -10,34 +10,34 @@ var productStartDate = "#hdStartDate";
 var productEndDate = "#hdEndDate";
 
 function extractItem(item, date, $) {
-    var extract = function(search) {
-	return $(item).find(search)['0'].attribs.value;
+    var extract = function (search) {
+        return $(item).find(search)['0'].attribs.value;
     }
     return {'name': extract(productname),
-	    'description': extract(productDescription),
-	    'imageURL': extract(productImageURL),
-	    'startDate': extract(productStartDate),
-	    'endDate': extract(productEndDate),
-	    'date': date};
+        'description': extract(productDescription),
+        'imageURL': extract(productImageURL),
+        'startDate': extract(productStartDate),
+        'endDate': extract(productEndDate),
+        'date': date};
 }
 
 /*
-  Crawl function.
-  cb must accept one argument - the data.
-  data is in the form [{raw item}, {raw item}, {raw item}]
-*/
-function crawl(cb){
+ Crawl function.
+ cb must accept one argument - the data.
+ data is in the form [{raw item}, {raw item}, {raw item}]
+ */
+function crawl(cb) {
     var nettoCrawler = new Crawler({
-	maxConnections : 10,
-	// This will be called for each crawled page
-	callback : function (error, result, $) {
-	    var res = [];
-	    var isodate = new Date().toISOString();
-	    res = $(productlist).map(function(index, el) {
-		return [extractItem(el, isodate, $)];
-	    });
-	    cb(res.get());
-	}
+        maxConnections: 10,
+        // This will be called for each crawled page
+        callback: function (error, result, $) {
+            var res = [];
+            var isodate = new Date().toISOString();
+            res = $(productlist).map(function (index, el) {
+                return [extractItem(el, isodate, $)];
+            });
+            cb(res.get());
+        }
     });
 
     nettoCrawler.queue(site);
@@ -45,6 +45,6 @@ function crawl(cb){
 }
 
 module.exports = {crawl: crawl,
-		  extractItem: extractItem}
+    extractItem: extractItem}
 
 
