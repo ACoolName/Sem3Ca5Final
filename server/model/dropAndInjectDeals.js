d1954dac32985374e7707c67a4deddede522b96b
TeodorDumitru <TeoDum@gmail.com>
2014-12-04 17:28:00 +0100

    Added irma injector.

--- server/model/dropAndInjectDeals.js
+++ server/model/dropAndInjectDeals.js
@@ -1,22 +1,26 @@
 /*
-  Front-end script that drops the collection and calls
-  the crawlers for the deals to fill the db.
-*/
+ Front-end script that drops the collection and calls
+ the crawlers for the deals to fill the db.
+ */
 var db = require("./db");
 var mongoose = require('mongoose');
 var Product = mongoose.model('Product');
 var netto = require("./nettoInject");
 var fakta = require("./faktaInject");
+var irma = require("./irmaInject");
 
-function doneLog(data){
-    if (data != null){
-	console.log("done! Inserting objs:" + data.length);
+function doneLog(data) {
+    console.log("End " + Date.now());
+    if (data != null) {
+        console.log("done! Inserting objs:" + data.length);
     } else {
-	console.log("error!");
+        console.log("error!");
     }
 }
 
-Product.collection.drop(function(){
+Product.collection.drop(function () {
+    console.log("Start " + Date.now());
     netto.start(doneLog);
     fakta.start(doneLog);
+    irma.start(doneLog);
 });
---
+++ server/model/irmaInject.js
@@ -0,0 +1,50 @@
+var db = require("./db");
+var mongoose = require('mongoose');
+var Product = mongoose.model('Product');
+var objectTranslator = require("../services/objectTranslator");
+var request = require('request');
+
+var injector = {};
+
+injector.start = function (callback) {
+    injector.inject(callback);
+};
+
+function extractUnit(priceAndUnit) {
+    var res = priceAndUnit.split(" ");
+    return res[res.length];
+}
+
+injector.inject = function (callback) {
+    request('https://irma.dk/api/catalog/search?Offer=HasOffer&page=1&pageSize=100000&format=json', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var jsonBody = JSON.parse(body);
+            var products = jsonBody.model.products;
+
+            var arr = [];
+
+            products.forEach(function (entry) {
+                var p = {};
+                p.origin = 4;  //origin 4 => Irma
+                p.title = entry.displayName;
+                p.imageLink = entry.freeTextImageUrl;   //freeTextImageUrl
+//                p.startDate;
+//                p.endDate;
+//                p.class;
+                p.date = Date.now();
+                p.unit = extractUnit(entry.pricePerUnitText);         //pricePerUnitText
+                p.price = entry.salesPrice.value;  //sales price{value}
+
+                arr.push(p);
+            });
+
+            objectTranslator.translate(arr, "title", function () {
+                Product.create(arr, function () {
+                    callback(arr);
+                });
+            });
+        }
+    });
+};
+
+module.exports = injector;
